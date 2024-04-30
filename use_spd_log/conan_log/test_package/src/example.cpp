20730acd58d427c99e15ffdf8df6c3f2f465c92e
Dai Li <d.li@simplylive.tv>
2024-04-30 16:58:24 +0800

    add test APP for spdlog

---
+++ use_spd_log/conan_log/test_package/src/example.cpp
@@ -0,0 +1,9 @@
+#include "loglib.h"
+#include <vector>
+#include <string>
+
+int main() {
+    SimplyLive::Logger*logger = new SimplyLive::Logger(L"c:\\ll\\example.log");
+
+return 0;
+}
---
+++ use_spd_log/use_conan_log/src/example.cpp
@@ -0,0 +1,9 @@
+#include "loglib.h"
+#include <vector>
+#include <string>
+
+int main() {
+    SimplyLive::Logger *logger = new SimplyLive::Logger(L"c:\\lo\\g.log");
+
+    return 0;
+}
